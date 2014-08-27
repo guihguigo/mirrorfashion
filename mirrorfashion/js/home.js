2b17a4d15a845902e69c3b735265255f96b403cd
Guilherme Alves Vargas <guilherme.alves.vargas@outlook.com>
2014-08-27 13:20:35 -0300

    Loja virtual mirrorfashion

---
+++ mirrorfashion/js/home.js
@@ -0,0 +1,34 @@
+//fazendo a associação da função com o evento
+document.querySelector('#form-busca').onsubmit = function() {
+	if (document.querySelector('#q').value == '') {
+		document.querySelector('#form-busca').style.background = 'red';
+		return false;
+	}
+};
+
+var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
+var bannerAtual = 0;
+
+function trocaBanner() {
+	bannerAtual = (bannerAtual + 1) % 2;
+	document.querySelector('.destaque img').src = banners[bannerAtual];
+}
+
+var timer = setInterval(trocaBanner, 4000);
+
+var controle = document.querySelector('.pause');
+
+controle.onclick = function() {
+	if (controle.className == 'pause') {
+		clearInterval(timer);
+		controle.className = 'play';
+	} else {
+		timer = setInterval(trocaBanner, 4000);
+		controle.className = 'pause';
+	}
+	
+	return false;
+};
+
+
+
